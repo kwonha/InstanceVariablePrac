4b6685fa717065db5d1d83dfcff58b857527a263
Haeun Kwon <s2118@e-mirim.hs.kr>
2023-05-09 11:47:30 +0900

    list에 인스턴스 생성해서 초기화하기 2 및 ToString 재정의 하고 list출력

--- InstanceVariablePrac/Program.cs
+++ InstanceVariablePrac/Program.cs
@@ -25,10 +25,35 @@ namespace InstanceVariablePrac
             Console.WriteLine(product1.name + " / " + product1.price);
             Console.WriteLine(product1);
 
+            Console.WriteLine();
+
             List<Student> list = new List<Student>();
             list.Add(new Student() { name = "곽경희", grade = 3 });
             list.Add(new Student() { name = "권하은", grade = 2 });
             list.Add(new Student() { name = "김선혜", grade = 1 });
+
+            List<Student> list2 = new List<Student>()
+            {
+                new Student() { name = "곽경희", grade = 3 },
+                new Student() { name = "권하은", grade = 2 },
+                new Student() { name = "김선혜", grade = 1 }, 
+        };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+
+
+
         }
     }
 }
